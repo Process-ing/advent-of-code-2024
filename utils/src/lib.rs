8c5b802ac4744556df58e03c0e9f8cf2acf72603
Process-ing <rbruno.p.o@gmail.com>
2024-12-02 10:30:08 +0000

    Day 2

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,6 +1,6 @@
 use std::io::{self, BufRead};
 
-fn parse_input() -> (Vec<i32>, Vec<i32>) {
+pub fn parse_two_lists() -> (Vec<i32>, Vec<i32>) {
     let (mut list1, mut list2) = (vec![], vec![]);
 
     let stdin = io::stdin();
@@ -15,24 +15,18 @@ fn parse_input() -> (Vec<i32>, Vec<i32>) {
         list2.push(nums[1]);
     }
 
-
     return (list1, list2);
 }
 
-fn solve(mut list1: Vec<i32>, mut list2: Vec<i32>) -> i32 {
-    list1.sort();
-    list2.sort();
+pub fn parse_int_lists() -> Vec<Vec<i32>> {
+    let stdin = io::stdin();
+    let lines = stdin.lock().lines().map(|line| line.unwrap());
 
-    let distance = list1.into_iter().zip(list2.into_iter())
-        .map(|(num1, num2)| (num1 - num2).abs())
-        .sum();
+    let lists: Vec<Vec<i32>> = lines.map(|line| {
+        line.split_whitespace()
+            .map(|word| word.parse().unwrap())
+            .collect()
+    }).collect();
 
-    return distance;
-}
-
-fn main() {
-    let (list1, list2) = parse_input();
-    let result = solve(list1, list2);
-
-    println!("{}", result);
+    return lists;
 }
