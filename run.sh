7fb68209847519eb0e1f8c5b465df44b6cb7d5c3
Process-ing <rbruno.p.o@gmail.com>
2024-12-03 23:33:18 +0000

    Improve run script

--- run.sh
+++ run.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 
-day=$(printf "%02d" $1)
-cargo run --release -p day$day < input/day$day
+usage() {
+    echo "Usage: $0 -d <day> [-i <input>]" 1>&2
+    exit 1
+}
+
+while getopts ":d:i:" opt; do
+  case $opt in
+    d)
+        day=$(printf "%02d" $OPTARG)
+        ;;
+    i)
+        input=$OPTARG
+        ;;
+    *)
+        usage
+        ;;
+  esac
+done
+
+if [ -z "$day" ]; then
+    usage
+fi
+if [ -z "$input" ]; then
+    input="input/day$day"
+fi
+
+cargo run --release -p day$day < $input
