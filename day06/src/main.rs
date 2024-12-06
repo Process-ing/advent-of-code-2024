b8b7ef0c23cdeab49daaf686954e09cd97f97422
Process-ing <rbruno.p.o@gmail.com>
2024-12-06 18:47:57 +0000

    Refactor day 6

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,9 +1,34 @@
 use utils::read_byte_matrix;
 
+const EMPTY: u8 = 0;
+const VISITED_UP: u8 = 1 << 0;
+const VISITED_RIGHT: u8 = 1 << 1;
+const VISITED_DOWN: u8 = 1 << 2;
+const VISITED_LEFT: u8 = 1 << 3;
+const OBSTACLE: u8 = 1 << 4;
+const START: u8 = 1 << 5;
+
+fn to_area_byte(cell: u8) -> u8 {
+    match cell {
+        b'.' => EMPTY,
+        b'#' => OBSTACLE,
+        b'^' => START,
+        _ => panic!("Invalid cell")
+    }
+}
+
+fn convert_area(matrix: &[Vec<u8>]) -> Vec<Vec<u8>> {
+    return matrix.iter()
+        .map(|line| line.iter()
+            .map(|&b| to_area_byte(b))
+            .collect())
+        .collect()
+}
+
 fn get_start(area: &[Vec<u8>]) -> (i32, i32) {
     return area.iter().enumerate()
         .flat_map(|(y, l)| l.iter().enumerate().map(move |(x, v)| (x, y, v)))
-        .find(|(_, _, &v)| v == b'^')
+        .find(|(_, _, &v)| v == START)
         .map(|(x, y, _)| (x as i32, y as i32))
         .unwrap()
 }
@@ -15,7 +40,7 @@ fn in_bounds(area: &[Vec<u8>], x: i32, y: i32) -> bool {
 }
 
 fn area_move(area: &[Vec<u8>], x: i32, y: i32, dx: i32, dy: i32) -> (i32, i32, i32, i32, bool) {
-    if area[(y + dy) as usize][(x + dx) as usize] == b'#' {
+    if area[(y + dy) as usize][(x + dx) as usize] == OBSTACLE {
         (x, y, -dy, dx, true)
     } else {
         (x + dx, y + dy, dx, dy, false)
@@ -24,24 +49,24 @@ fn area_move(area: &[Vec<u8>], x: i32, y: i32, dx: i32, dy: i32) -> (i32, i32, i
 
 fn direction_to_bit(dx: i32, dy: i32) -> u8 {
     match (dx, dy) {
-        (0, -1) => 0b0001,
-        (1, 0) => 0b0010,
-        (0, 1) => 0b0100,
-        (-1, 0) => 0b1000,
-        _ => 0 
+        (0, -1) => VISITED_UP,
+        (1, 0) => VISITED_RIGHT,
+        (0, 1) => VISITED_DOWN,
+        (-1, 0) => VISITED_LEFT,
+        _ => panic!("Invalid direction"),
     }
 }
 
 fn calculate_path(area: &mut [Vec<u8>]) -> i32 {
     let mut count = 1;
     let (mut x, mut y) = get_start(area);
-    area[y as usize][x as usize] = b'X';
+    area[y as usize][x as usize] = direction_to_bit(0, -1);
 
     let (mut dx, mut dy) = (0, -1);  // Up
     while in_bounds(area, x + dx, y + dy) {
         (x, y, dx, dy, _) = area_move(area, x, y, dx, dy);
 
-        if area[y as usize][x as usize] == b'.' {
+        if area[y as usize][x as usize] == EMPTY {
             count += 1;
             area[y as usize][x as usize] = direction_to_bit(dx, dy);
         } else {
@@ -55,19 +80,29 @@ fn calculate_path(area: &mut [Vec<u8>]) -> i32 {
     return count;
 }
 
-fn count_cycles(area: &mut [Vec<u8>]) -> i32 {
+fn is_visited(cell: u8) -> bool {
+    cell & 0b1111 != 0
+}
+
+fn adjacent_to_visited(area: &[Vec<u8>], x: i32, y: i32) -> bool {
+    [(0, 0), (0, 1), (1, 0), (-1, 0), (0, -1)].into_iter()
+        .map(|(dx, dy)| (x + dx, y + dy))
+        .filter(|&(x, y)| in_bounds(area, x, y))
+        .any(|(x, y)| is_visited(area[y as usize][x as usize]))
+} 
+
+fn count_cycles(area: &[Vec<u8>], travelled_area: &[Vec<u8>]) -> i32 {
     let mut count = 0;
 
     for y in 0..area.len() as i32 {
         for x in 0..area[y as usize].len() as i32 {
-            if area[y as usize][x as usize] != b'.' {
+            if area[y as usize][x as usize] != EMPTY || !adjacent_to_visited(travelled_area, x, y) {
                 continue;
             }
 
-            area[y as usize][x as usize] = b'#';
             let mut temp_area: Vec<Vec<u8>> = area.iter().map(|x| x.clone()).collect();
+            temp_area[y as usize][x as usize] = OBSTACLE;
             count += (calculate_path(&mut temp_area) == -1) as i32; 
-            area[y as usize][x as usize] = b'.';
         }
     }
 
@@ -75,10 +110,11 @@ fn count_cycles(area: &mut [Vec<u8>]) -> i32 {
 }
 
 fn main() {
-    let mut matrix = read_byte_matrix();
+    let area = convert_area(&read_byte_matrix());
+    let mut travelled_area =  area.clone();
 
-    let result1 = calculate_path(&mut matrix.clone());
-    let result2 = count_cycles(&mut matrix);
+    let result1 = calculate_path(&mut travelled_area);
+    let result2 = count_cycles(&area, &travelled_area);
 
     println!("Part 1 result: {result1}");
     println!("Part 2 result: {result2}");
